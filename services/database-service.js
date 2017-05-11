cb4e11d75019e1a8ad83c3d6f7930d8a2baeca92
finnatinform <finn.zentgraf@inform-software.com>
2017-05-11 21:43:13 +0200

    Datenbank BuildUp weiter ausgebaut

--- services/database-service.js
+++ services/database-service.js
@@ -2,7 +2,17 @@ module.exports = {
     BuildDatabase : function(_Database){
         var HResult = "Datenbank wurde erfolgreicht aufgebaut.";
         _Database.serialize(function() {            
-            _Database.run("CREATE TABLE if not exists USERS (IDENT INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)");
+            // Create USERS Table
+            _Database.run("CREATE TABLE if not exists USERS (IDENT INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, COMPANY_IDENT INTEGER, FOREIGN KEY(COMPANY_IDENT) REFERENCES COMPANIES(IDENT))");
+            // Create FAIRS Table
+            _Database.run("CREATE TABLE if not exists FAIRS (IDENT TEXT PRIMARY KEY NOT NULL UNIQUE, CAPTION TEXT)");
+
+            // Create REFERENTS Table
+            _Database.run("CREATE TABLE if not exists FAIRS (IDENT INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, FORE_NAME TEXT, SURE_NAME TEXT)");
+
+            // Create COMPANIES Table
+            _Database.run("CREATE TABLE if not exists COMPANIES (IDENT INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, SHORT_NAME TEXT UNIQUE, CAPTION TEXT)");
+
 
         });
         return HResult ;
