eaea8bed514a20c92db66e5ae7924f9d192b6cbe
finnatinform <finn.zentgraf@inform-software.com>
2017-09-06 19:13:26 +0200

    Fehler korrigiert

--- services/tables/stand-service.js
+++ services/tables/stand-service.js
@@ -2,7 +2,7 @@ module.exports = {
     addStand(_DataBase, _Stand, _Callback){
         _DataBase.serialize(
             function(){
-                _DataBase.run("INSERT INTO STANDS (TITLE,DESCRIPTION,LOGO,RANKING) VALUES (?,?,?,?)", [ _Stand.TITLE, _Stand.DESCRIPTION, _Stand.LOGO, _Stand.RANKING ], function(_Error){
+                _DataBase.run("INSERT INTO STANDS (TITLE,DESCRIPTION,LOGO,RANKING) VALUES (?,?,?,?)", [ _Stand.CAPTION, _Stand.DESCRIPTION, _Stand.LOGO, _Stand.RANKING ], function(_Error){
                     var HResult = "";
                     if(_Error === null){
                         HResult = 'success';
@@ -18,7 +18,7 @@ module.exports = {
     changeStand(_DataBase, _Stand, _Callback){
         _DataBase.serialize(
             function(){
-                _DataBase.run("UPDATE STANDS SET TITLE=?,SURE_NAME=?,DESCRIPTION=?,LOGO=?,RANKING=? WHERE IDENT=?", [ _Stand.TITLE, _Stand.DESCRIPTION, _Stand.LOGO,_Stand.RANKING, _Stand.IDENT ], function(_Error){
+                _DataBase.run("UPDATE STANDS SET TITLE=?,SURE_NAME=?,DESCRIPTION=?,LOGO=?,RANKING=? WHERE IDENT=?", [ _Stand.CAPTION, _Stand.DESCRIPTION, _Stand.LOGO,_Stand.RANKING, _Stand.IDENT ], function(_Error){
                     var HResult = "";
                     if(_Error === null){
                         HResult = 'success';
